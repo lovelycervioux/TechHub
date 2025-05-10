93135fc3c859b0b855c759e472e6c488dc8c7af8
lovelycervioux <lovelyjoromo12@gmail.com>
2025-05-10 14:01:01 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/your-repo-name/',  // <-- IMPORTANT
+  base: '/TechHub/',  // <-- IMPORTANT
   plugins: [react()],
 })
 
