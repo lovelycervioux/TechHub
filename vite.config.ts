dd4dd94b86312bd1558da2bdc2df9e362c7c2dba
lovelycervioux <lovelyjoromo12@gmail.com>
2025-05-10 14:04:41 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   base: '/TechHub/',  // <-- IMPORTANT
-  plugins: [react()],
-})
+  plugins: [
+    react(),
 
     {
       name: "mocha-error-reporter",
